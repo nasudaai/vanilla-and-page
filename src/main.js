4a70ed554246567b251c40df4c43de82343acf5e
nasuda <nasuda908@gmail.com>
2025-03-20 09:09:47 +0900

    add livereload code and change css jscode

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-alert("test")
+new EventSource('/esbuild').addEventListener('change', () => location.reload())
+
+alert("test-a")
+
+const headChange = document.querySelector("h1")
+headChange.style.color = "red"
+
+
