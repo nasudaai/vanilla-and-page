431dd969855c3581d6235e7381ff9b361cc33612
nasuda <nasuda908@gmail.com>
2025-03-31 08:29:32 +0900

    delete devcode shellscript

---
+++ index.js
@@ -0,0 +1,19 @@
+
+import {app, appTest, appH1, para, btn, btn2} from "./main.js"
+import {app2}  from "./app.js"
+
+
+alert("test-a")
+
+const headChange = document.querySelector("h1")
+headChange.style.color = "red"
+
+
+appTest()
+console.log(appH1)
+console.log (app2)
+
+app.appendChild(appH1)
+app.appendChild(para)
+app.appendChild(btn2)
+console.log(btn)
