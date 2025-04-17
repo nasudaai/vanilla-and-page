9f666a98ecb22e48cb9e126b2448df1b726fda24
nasuda <nasuda908@gmail.com>
2025-04-17 09:54:10 +0900

    test return element function

--- client-render/src/app.js
+++ client-render/src/app.js
@@ -2,5 +2,7 @@ import header from './components/header.js'
 
 export const app = document.getElementById("app")
 
+const subTitle = document.createElement("h2")
+
 app.appendChild(header)
 
--- client-render/src/main.js
+++ client-render/src/main.js
@@ -2,4 +2,13 @@ import { app } from './app.js'
 alert("test")
 
 
+function createPara() {
+  const para = document.createElement("p")
+  return para
+}
+
+const para = createPara()
+
+
 console.log(app)
+console.log(para)
